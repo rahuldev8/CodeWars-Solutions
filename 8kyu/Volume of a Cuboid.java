33e59fb42721f8f02676cc2c6afc69f1e33f11e5
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 19:01:17 +0530

    Volume of a Cuboid
    Problem link to try it yourself : https://www.codewars.com/kata/58261acb22be6e2ed800003a

---
+++ 8kyu/Volume of a Cuboid.java
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+Bob needs a fast way to calculate the volume of a 
+cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.*/
+
+public class Kata {
+
+  public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    // Your code here
+    double ans = length*width*height;
+    return ans;
+  }
+  
+}
