ed781ff794948cfe0982c2f92d557cc93cf416b9
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 18:45:11 +0530

    Multiply
    Problem Link to try it Yourself :  https://www.codewars.com/kata/50654ddff44f800200000004

---
+++ 8kyu/Multiply.java
@@ -0,0 +1,8 @@
+/*
+This code does not execute properly. Try to figure out why.*/
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
