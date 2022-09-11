34117c02c3f2d714c723eef018cd9867c728d3f4
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 18:48:14 +0530

    Convert a Number to a String!
    Problem link to try it Yourself : https://www.codewars.com/kata/5265326f5fda8eb1160004c8

---
+++ 8kyu/Convert a Number to a String!.java
@@ -0,0 +1,19 @@
+/*
+We need a function that can transform a number (integer) into a string.
+
+What ways of achieving this do you know?
+
+In C, return a dynamically allocated string that will be freed by the test suite.
+
+Examples (input --> output):
+123  --> "123"
+999  --> "999"
+-100 --> "-100"*/
+
+class Kata {
+  public static String numberToString(int num) {
+    // Return a string of the number here!
+    String ans = String.valueOf(num);
+    return ans;
+  }
+}
