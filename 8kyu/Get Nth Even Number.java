b338fba20c245f72a76fbcea837b0aa43e15fb02
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 18:54:32 +0530

    Get Nth Even Number
    Problem link to try it yourself : https://www.codewars.com/kata/5933a1f8552bc2750a0000ed

---
+++ 8kyu/Get Nth Even Number.java
@@ -0,0 +1,20 @@
+/*
+DESCRIPTION:
+Return the Nth Even Number
+
+Example(Input --> Output)
+
+1 --> 0 (the first even number is 0)
+3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+100 --> 198
+1298734 --> 2597466
+The input will not be 0.*/
+
+
+public class Num {
+  public static int nthEven(int n) {
+  // write your code here
+    int ans = (n*2)-2;
+    return ans;
+  }
+}
