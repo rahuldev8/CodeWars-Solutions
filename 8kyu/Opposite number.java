f39a4ac7b926a7fdd2a78c8c15499c6a6266c2e1
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 18:49:27 +0530

    Opposite number
    Problem link to try it yourself : https://www.codewars.com/kata/56dec885c54a926dcd001095

---
+++ 8kyu/Opposite number.java
@@ -0,0 +1,18 @@
+/*
+Very simple, given an integer or a floating-point number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34*/
+
+
+public class Kata
+    {
+        public static int opposite(int number)
+        {
+            int ans = number*-1;
+          return ans;
+        }
+    }
