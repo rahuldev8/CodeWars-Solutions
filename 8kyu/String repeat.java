6408cec7b36e2bf3c4c344df6cbba34fea22ff9f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 18:58:26 +0530

    String repeat
    Problem link to try it yourself : https://www.codewars.com/kata/57a0e5c372292dd76d000d7e

---
+++ 8kyu/String repeat.java
@@ -0,0 +1,18 @@
+/*
+DESCRIPTION:
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"*/
+
+public class Solution {
+    public static String repeatStr(final int repeat, final String string) {
+      String ans = "";
+      for(int i=0;i<repeat;i++)
+        {
+        ans += string;
+      }
+        return ans;
+    }
+}
