80dee455b0dc035045925d53b0874e2c613fda15
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 18:51:56 +0530

    Remove First and Last Character
    Problem link to try it yourself : https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0

---
+++ 8kyu/Remove First and Last Character.java
@@ -0,0 +1,14 @@
+/*
+DESCRIPTION:
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+
+*/
+
+public class RemoveChars {
+    public static String remove(String str) {
+      String ans = str.substring(1,str.length()-1);
+      return ans;
+
+        // your code here
+    }
+}
