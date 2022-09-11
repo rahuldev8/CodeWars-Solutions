9fe3fb6f1927af9fcd6759c635ff384ccbae8340
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 18:53:17 +0530

    Even or Odd
    Problem link to try it yourself : https://www.codewars.com/kata/53da3dbb4a5168369a0000fe

---
+++ 8kyu/Even or Odd.java
@@ -0,0 +1,13 @@
+/*
+DESCRIPTION:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.*/
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        //Place code here
+      String ans;
+      if(number%2 == 0)ans = "Even";
+      else ans = "Odd";
+      return ans;
+    }
+}
