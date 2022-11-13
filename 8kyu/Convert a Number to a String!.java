6a874eaf87f84cf079c7be602d034b0d06e2c510
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-11-13 18:11:07 +0530

    Update Convert a Number to a String!.java

--- 8kyu/Convert a Number to a String!.java
+++ 8kyu/Convert a Number to a String!.java
@@ -12,7 +12,7 @@ Examples (input --> output):
 
 class Kata {
   public static String numberToString(int num) {
-    // Return a string of the number here!
+    
     String ans = String.valueOf(num);
     return ans;
   }
