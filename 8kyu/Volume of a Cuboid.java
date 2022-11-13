6237b0410fa2f47121e797d3b58aebc3bf1d747f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-11-13 18:08:03 +0530

    Update Volume of a Cuboid.java

--- 8kyu/Volume of a Cuboid.java
+++ 8kyu/Volume of a Cuboid.java
@@ -6,7 +6,7 @@ cuboid with three values: the length, width and height of the cuboid. Write a fu
 public class Kata {
 
   public static double getVolumeOfCuboid(final double length, final double width, final double height) {
-    // Your code here
+   
     double ans = length*width*height;
     return ans;
   }
