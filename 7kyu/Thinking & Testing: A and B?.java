af0fcb04efd8b744dc1cc94b39af50144486974d
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-11-27 18:50:17 +0530

    Thinking & Testing: A and B?
    Problem Link to try it Yourself :  https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/java

---
+++ 7kyu/Thinking & Testing: A and B?.java
@@ -0,0 +1,22 @@
+/*
+No Story
+
+No Description
+
+Only by Thinking and Testing
+
+Look at result of testcase, guess the code!
+
+*/
+
+public class ThinkingAndTesting {
+
+  public static int testAB(int a, int b) {
+    // Good luck!
+    //Bitwise or Operator
+    return a | b; // hmm ...is that right?
+    
+    //Time Complexity : o(1)
+    //Space Complexity : o(1)
+  }
+}
